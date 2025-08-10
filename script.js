9626213ae8bcd371bea3391760cecec74f6578ce
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-08-10 15:28:20 -0400

    Fix: change for loop max iteration to 255
    Changed the max iteration of the for loop to 255
    instead of 256 since it created an additional div

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i <= 256; i++) {
+for (i = 0; i <= 255; i++) {
     let div = document.createElement("div");
+    div.classList.add("square");
     container.appendChild(div);
 }
