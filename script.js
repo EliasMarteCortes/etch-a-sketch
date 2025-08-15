1d8d02c06298c7b20f310a71a447fcc8dc93c13a
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-08-15 10:01:53 -0400

    Fix: put code to generate squares into a function
    Placed the code to generate squares inside the
    grid into a function to prevent repetition of code
    since it will be used again in the code

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i <= 255; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+function fillGrid(numSquares) {
+
+    numSquares = numSquares ** 2;
+    for (i = 1; i <= numSquares; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+    return;
 }
+
+fillGrid(16);
 
 container.addEventListener("mouseover",(e) => {
     let target = e.target;
