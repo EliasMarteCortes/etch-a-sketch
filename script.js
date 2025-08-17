8527bc888821db36435470abaf047fd2387c618a
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-08-17 08:34:43 -0400

    Add function to clear the grid of squares
    This function will be called when the user tries
    to change the size of the grid by clicking the
    change size button

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ function fillGrid(numSquares) {
     return;
 }
 
+function clearGrid() {
+    const divs = document.querySelectorAll(".square");
+    for (let i = 0; i < divs.length; i++) {
+        container.removeChild(divs[i]);
+    }
+}
+
 fillGrid(16);
 
 container.addEventListener("mouseover",(e) => {
@@ -27,5 +34,11 @@ container.addEventListener("mouseover",(e) => {
 
 button.addEventListener("click",() => {
     let userGridSize = prompt("Enter grid size (e.g., 16 for 16 x 16) must not exceed 100:", 16);
-
+    if (userGridSize <= 100){
+        clearGrid();
+        fillGrid(userGridSize);
+    }
+    else {
+        alert("Grid size should not be greater than 100.");
+    }
 })
